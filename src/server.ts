f9f50a34b88b3bb2520c27c114aca41e46730985
Alex Werner <alexanderdwerner@gmail.com>
2020-11-30 11:53:30 -0600

    bug: moves redis to entrypoint for deno permissions

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
-import { serve } from "https://deno.land/std@0.79.0/http/server.ts";
+import {
+  serve,
+  ServerRequest,
+} from "https://deno.land/std@0.79.0/http/server.ts";
 import {
   acceptWebSocket,
-  isWebSocketCloseEvent,
-  isWebSocketPingEvent,
   WebSocket,
 } from "https://deno.land/std@0.79.0/ws/mod.ts";
+import { connect } from "https://deno.land/x/redis/mod.ts";
+import { interpret } from "./events.ts";
+import type { RoomEvent } from "./types.ts";
+
+const redis = await connect({
+  hostname: Deno.args[1] ? Deno.args[1] : "127.0.0.1",
+  port: 6379,
+});
 
 async function handleWs(ws: WebSocket) {
   console.log("socket connected!");
@@ -12,20 +21,12 @@ async function handleWs(ws: WebSocket) {
   try {
     for await (const ev of ws) {
       if (typeof ev === "string") {
-        // text message.
-        console.log("ws:Text", ev);
+        // Try to parse into json
+        const jsonEvent: RoomEvent = JSON.parse(ev);
+        console.log(`Socket sent: ${ev} \n`);
+        interpret(redis, jsonEvent);
+
         await ws.send(ev);
-      } else if (ev instanceof Uint8Array) {
-        // binary message.
-        console.log("ws:Binary", ev);
-      } else if (isWebSocketPingEvent(ev)) {
-        const [, body] = ev;
-        // ping.
-        console.log("ws:Ping", body);
-      } else if (isWebSocketCloseEvent(ev)) {
-        // close.
-        const { code, reason } = ev;
-        console.log("ws:Close", code, reason);
       }
     }
   } catch (err) {
@@ -37,26 +38,28 @@ async function handleWs(ws: WebSocket) {
   }
 }
 
+async function handleStatic(req: ServerRequest) {
+  const _file = await Deno.readFile("public/index.html");
+  const decoder = new TextDecoder();
+
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/html",
+    }),
+    body: decoder.decode(_file),
+  });
+}
+
 if (import.meta.main) {
   /** websocket echo server */
-  const port = 8080;
+  const port = Deno.args[0] ? Number(Deno.args[0]) : 8080;
   console.log(`websocket server is running on :${port}`);
 
   for await (const req of serve({ port })) {
     const { conn, r: bufReader, w: bufWriter, headers } = req;
-    const _file = await Deno.readFile("public/index.html");
-    const decoder = new TextDecoder();
 
-    if (!req.headers.has("upgrade")) {
-      console.log("Requested static html");
-      await req.respond({
-        status: 200,
-        headers: new Headers({
-          "content-type": "text/html",
-        }),
-        body: decoder.decode(_file),
-      });
-    } else {
+    if (req.headers.has("upgrade")) {
       console.log("Requested websocket conn");
       acceptWebSocket({
         conn,
@@ -70,6 +73,8 @@ if (import.meta.main) {
           console.log(`failed request of type: ${req.method}`);
           await req.respond({ status: 400 });
         });
+    } else {
+      await handleStatic(req);
     }
   }
 }
